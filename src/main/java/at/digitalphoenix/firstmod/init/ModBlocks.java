288215c60be7b5872ec522918effb1a697dd5cb5
Benjamin Bachmayr <Benjamin Bachmayr@Corsair380t>
2018-02-11 19:28:28 +0100

    fixed a merge bug

--- src/main/java/at/digitalphoenix/firstmod/init/ModBlocks.java
+++ src/main/java/at/digitalphoenix/firstmod/init/ModBlocks.java
@@ -14,10 +14,7 @@ public class ModBlocks {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
 	public static final Block FIRE_RUNE_BLOCK = new BlockBase("fire_rune_block", Material.IRON);
-<<<<<<< HEAD
-	public static final Block FIRE_LAMP_BLOCK = new FireLamp("fire_lamp_block", Material.IRON, 15);
-=======
 	
-	public static final Block FIRE_LAMP_BLOCK = new FireLamp("fire_lamp_block", Material.IRON, 16);
->>>>>>> branch 'master' of https://github.com/Ineb01/FirstMinecraftMod.git
+	public static final Block FIRE_LAMP_BLOCK = new FireLamp("fire_lamp_block", Material.IRON, 15);
+
 }
